5aa100b22f297bb042ac1d1e580845423cc4ced1
andi faizal <andifaizal@MacBook-Pro-andi.local>
2024-12-27 14:23:47 +0700

    challenge: domain url

---
+++ domain.js
@@ -0,0 +1,11 @@
+function domainName(url) {
+  let domain = url.replace(/(^\w+:|^)\/\//, "");
+
+  domain = domain.replace(/^www\./, "");
+
+  domain = domain.split("/")[0];
+
+  domain = domain.split(".")[0];
+
+  return domain;
+}
